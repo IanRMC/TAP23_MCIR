5958d515f1fdd33c15ed9ff150fe18645b247dc5
IanRMC <98826018+IanRMC@users.noreply.github.com>
2023-03-20 17:33:42 -0600

    Add files via upload

---
+++ Hilos_Corredores/Hilos_Corredores/Corredores.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Hilos_Corredores
+{
+    partial class Corredores
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.buttonIniciar1 = new System.Windows.Forms.Button();
+            this.buttonIniciar2 = new System.Windows.Forms.Button();
+            this.buttonIniciar3 = new System.Windows.Forms.Button();
+            this.buttonIniciarTodos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hilos_Corredores.Properties.Resources.among_us_gif_8;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Hilos_Corredores.Properties.Resources.among_us_gif_8;
+            this.pictureBox2.Location = new System.Drawing.Point(39, 139);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 111);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Hilos_Corredores.Properties.Resources.among_us_gif_8;
+            this.pictureBox3.Location = new System.Drawing.Point(39, 256);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 111);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // buttonIniciar1
+            // 
+            this.buttonIniciar1.Location = new System.Drawing.Point(42, 399);
+            this.buttonIniciar1.Name = "buttonIniciar1";
+            this.buttonIniciar1.Size = new System.Drawing.Size(75, 23);
+            this.buttonIniciar1.TabIndex = 3;
+            this.buttonIniciar1.Text = "Iniciar 1";
+            this.buttonIniciar1.UseVisualStyleBackColor = true;
+            this.buttonIniciar1.Click += new System.EventHandler(this.buttonIniciar1_Click);
+            // 
+            // buttonIniciar2
+            // 
+            this.buttonIniciar2.Location = new System.Drawing.Point(123, 399);
+            this.buttonIniciar2.Name = "buttonIniciar2";
+            this.buttonIniciar2.Size = new System.Drawing.Size(75, 23);
+            this.buttonIniciar2.TabIndex = 4;
+            this.buttonIniciar2.Text = "Iniciar 2";
+            this.buttonIniciar2.UseVisualStyleBackColor = true;
+            this.buttonIniciar2.Click += new System.EventHandler(this.buttonIniciar2_Click);
+            // 
+            // buttonIniciar3
+            // 
+            this.buttonIniciar3.Location = new System.Drawing.Point(204, 399);
+            this.buttonIniciar3.Name = "buttonIniciar3";
+            this.buttonIniciar3.Size = new System.Drawing.Size(75, 23);
+            this.buttonIniciar3.TabIndex = 5;
+            this.buttonIniciar3.Text = "Iniciar 3";
+            this.buttonIniciar3.UseVisualStyleBackColor = true;
+            this.buttonIniciar3.Click += new System.EventHandler(this.buttonIniciar3_Click);
+            // 
+            // buttonIniciarTodos
+            // 
+            this.buttonIniciarTodos.Location = new System.Drawing.Point(42, 428);
+            this.buttonIniciarTodos.Name = "buttonIniciarTodos";
+            this.buttonIniciarTodos.Size = new System.Drawing.Size(237, 23);
+            this.buttonIniciarTodos.TabIndex = 6;
+            this.buttonIniciarTodos.Text = "Iniciar Todos";
+            this.buttonIniciarTodos.UseVisualStyleBackColor = true;
+            this.buttonIniciarTodos.Click += new System.EventHandler(this.buttonIniciarTodos_Click);
+            // 
+            // Corredores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.buttonIniciarTodos);
+            this.Controls.Add(this.buttonIniciar3);
+            this.Controls.Add(this.buttonIniciar2);
+            this.Controls.Add(this.buttonIniciar1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Corredores";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button buttonIniciar1;
+        private System.Windows.Forms.Button buttonIniciar2;
+        private System.Windows.Forms.Button buttonIniciar3;
+        private System.Windows.Forms.Button buttonIniciarTodos;
+    }
+}
+
---
+++ Hilos_Corredores/Hilos_Corredores/Corredores.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Hilos_Corredores
+{
+    public partial class Corredores : Form
+    {
+        static Random rnd = new Random();
+        private Thread t1, t2, t3;
+        delegate void StringArgReturningVoidDelegate(Form frm, PictureBox pb);
+
+        public Corredores()
+        {
+            InitializeComponent();
+        }
+
+        private void CorrerProceso(Form frm, PictureBox b)
+        {
+            int stp = 1;
+            if(b.InvokeRequired)
+            {
+                while (b.Left + b.Width + 20 < frm.Width)
+                {
+                    StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(CorrerProceso);
+                    this.Invoke(d, new object[] { frm, b });
+                    Thread.Sleep(rnd.Next(1, 80));
+                }
+            }else
+            {
+                b.Left += stp;
+            }
+        }
+
+        private void CorreHilo(out Thread hilo, PictureBox pb)
+        {
+            ThreadStart delegado = () => CorrerProceso(this, pb);
+            hilo = new Thread(delegado);
+            hilo.Start();
+        }
+
+        public void Multiple()
+        {
+            while (t1.IsAlive && t2.IsAlive && t3.IsAlive) ;
+
+            int ganador;
+            if(!t1.IsAlive)ganador = 1;
+            else if(!t2.IsAlive)ganador = 2;
+            else ganador = 3;
+            while(t1.IsAlive || t2.IsAlive || t3.IsAlive) ;
+            MessageBox.Show("El corredor " + ganador + " fue el ganador");
+        }
+
+        private void buttonIniciar1_Click(object sender, EventArgs e)
+        {
+            CorreHilo(out t1, pictureBox1);
+        }
+
+        private void buttonIniciar2_Click(object sender, EventArgs e)
+        {
+            CorreHilo (out t2, pictureBox2);
+        }
+
+        private void buttonIniciar3_Click(object sender, EventArgs e)
+        {
+            CorreHilo (out t3, pictureBox3);
+        }
+
+        private void buttonIniciarTodos_Click(object sender, EventArgs e)
+        {
+            CorreHilo(out t1, pictureBox1);
+            CorreHilo(out t2, pictureBox2);
+            CorreHilo(out t3, pictureBox3);
+
+            ThreadStart d1 = () => Multiple();
+            Thread hilo = new Thread(d1);
+            hilo.Start();
+        }
+    }
+}
