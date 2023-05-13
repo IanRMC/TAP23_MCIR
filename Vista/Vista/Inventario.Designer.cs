535ef986ce1447d3b9300d4532f9c600eb259b4f
Ian <s21120181@alumnos.itsur.edu.mx>
2023-05-12 18:39:58 -0600

    Implementación de un agregado de stock funcional

---
+++ Vista/Vista/Inventario.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Vista
+{
+    partial class Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbProductos = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnAniadir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbProductos
+            // 
+            this.cbProductos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbProductos.FormattingEnabled = true;
+            this.cbProductos.Location = new System.Drawing.Point(128, 56);
+            this.cbProductos.Name = "cbProductos";
+            this.cbProductos.Size = new System.Drawing.Size(182, 21);
+            this.cbProductos.TabIndex = 0;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(128, 122);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(182, 20);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // btnAniadir
+            // 
+            this.btnAniadir.Location = new System.Drawing.Point(94, 208);
+            this.btnAniadir.Name = "btnAniadir";
+            this.btnAniadir.Size = new System.Drawing.Size(153, 23);
+            this.btnAniadir.TabIndex = 2;
+            this.btnAniadir.Text = "Añadir ";
+            this.btnAniadir.UseVisualStyleBackColor = true;
+            this.btnAniadir.Click += new System.EventHandler(this.btnAniadir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cantidad";
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 289);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAniadir);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cbProductos);
+            this.Name = "Inventario";
+            this.Text = "Inventario";
+            this.Load += new System.EventHandler(this.Inventario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbProductos;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnAniadir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Vista/Vista/Inventario.cs
@@ -0,0 +1,96 @@
+﻿using Datos;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Vista
+{
+    public partial class Inventario : Form
+    {
+        private Product Productos;
+        ProductDAO productDAO = new ProductDAO();
+        public Inventario()
+        {
+            InitializeComponent();
+            CargarDatos();
+        }
+
+        public void CargarDatos() 
+        {
+            foreach(Product product in App.products) {
+                if(!product.Discontinued)
+                {
+                    cbProductos.Items.Add(product);
+                }
+            }
+
+            
+            cbProductos.DisplayMember = "ProductName";
+        }
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Inventario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAniadir_Click(object sender, EventArgs e)
+        {
+
+            int numProductos = 0;
+
+            if (cbProductos.SelectedIndex.Equals(""))
+            {
+                MessageBox.Show("Selecciona un Producto");
+            }
+            else if (txtCantidad.Text.Equals(""))
+            {
+                MessageBox.Show("Coloque una cantida de productos valida");
+            }
+            else
+            {
+                if (int.TryParse(txtCantidad.Text, out numProductos))
+                {
+                    Productos = (Product)cbProductos.SelectedItem;
+
+                    if((Productos.UnitsInStock + int.Parse(txtCantidad.Text)<= Productos.ReorderLevel * 5))
+                    {
+
+                        if(numProductos >= 1)
+                        {
+                            Productos.UnitsInStock += numProductos;
+                            productDAO.Update(Productos);
+                            this.Hide();
+                            App.huboCambio = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se admiten numeros negativos");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ya hay demaciado stock");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Coloque un valor valido");
+                }
+            }
+        }
+    }
+}
